f14cfd512228ff0216c494ce274da241565bad26
(Student) - James Doyle <C00252855@ITCARLOW.IE>
2022-10-10 15:46:00 +0100

    updated code

--- classwork/functions.py
+++ classwork/functions.py
@@ -1,18 +1,46 @@
+# create cube class
+class Cube:
+  def __init__(self, x, y, z):
+    self.x = x
+    self.y = y
+    self.z = z
 
-# Cube cordinate class
-class Coordinate:
-  def __init__(cube, x, y, z):
-    cube.x = x
-    cube.y = y
-    cube.z = z
+# create empty location list 
+locations_list = []
 
-coords = Coordinate(1, 1, 1)
-print(coords.x)
-print(coords.y) 
-print(coords.z) 
+# iniilise x, y, x, at 0 for loop later
+x = 0
+y = 0
+z = 0
 
-coords_list = [coords]
+# number of cubes to make
+index = 10    
 
-coords_list.append(Coordinate(3, 3, 3))
-for object in coords_list:
-    print("x:" + str(object.x) + " y:"+ str(object.y) + " z:"+ str(object.z))
+# fill location list array with cubes
+# x increases by 1 on each loop, '*' multiplier spaces out the cubes
+for x in range(index):
+  locations_list.append(Cube(3*x, y, z))
+
+# print the x, y, z coordinates from the location list array
+for object in locations_list:
+  print(f"{object.x}, {object.y}, {object.z}")
+
+# line break to make the array print outs more readable
+print("\n")
+
+# modify the y value of a cube
+locations_list[3] = (Cube(9, 3, z)) 
+
+# print the x, y, z coordinates from the ammended location list array
+for object in locations_list:
+  print(f"{object.x}, {object.y}, {object.z}")
+
+# line break to make the array print outs more readable
+print("\n")
+
+# delete a cube from the location list array
+locations_list.pop(7)
+
+# print the x, y, z coordinates from the ammended location list array
+for object in locations_list:
+  print(f"{object.x}, {object.y}, {object.z}")
